5994bf97b91ae184edc584f10d6ca79f37f4213f
Alexander Little <alittle44@fastmail.com>
2023-01-09 23:27:52 -0500

    implemented working pdf-to-word converter

---
+++ scripts/pdf-converter.js
@@ -0,0 +1,24 @@
+const convertButton = document.getElementById('convert-button')
+convertButton.addEventListener('click', () => {
+    const fileInput = document.getElementById('pdf-file-input')
+    const pdfFile = fileInput.files[0]
+    const formData = new FormData()
+    formData.append('pdf', pdfFile)
+
+    fetch('pdf-to-word-converter.alittle44.workers.dev/convert', {
+        method: 'POST',
+        body: formData
+    })
+        .then(response => {
+            if (response.ok) {
+                return response.blob().then(blob => URL.createObjectURL(blob))
+            } else {
+                console.error('Failed to convert PDF to DOCX')
+            }
+        })
+        .then(docxUrl => {
+            const downloadLink = document.getElementById('download-link')
+            downloadLink.style.display = 'inline'
+            downloadLink.href = docxUrl
+        })
+})
